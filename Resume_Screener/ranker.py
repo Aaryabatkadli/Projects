3f9d260314957ccb4c2bede5ad0327a3aff8ed11
Aarya Batkadli <150148918+Aaryabatkadli@users.noreply.github.com>
2026-01-20 23:24:11 +0530

    Delete Resume_Screener directory

--- Resume_Screener/ranker.py
+++
@@ -1,5 +0,0 @@
-def rank_resumes(results):
-    """
-    Sort resumes by similarity score (higher is better)
-    """
-    return sorted(results, key=lambda x: x["score"], reverse=True)
--- Resume_Screener/test.py
+++
@@ -1,3 +0,0 @@
-from gpt_analysis import analyze_resume
-
-print(analyze_resume("Python developer with 4 years experience in Django, SQL, AWS"))
